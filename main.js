047c238221d8383989ebcc3ef12d8e2de2158d66
Clifton Norman <152595919+comers-zen@users.noreply.github.com>
2023-12-02 17:35:27 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// Function defined in another file
+function showMessageFromHelper() {
+    console.log("This message comes from the helper JavaScript file.");
+}
+
+// Call the function
+showMessageFromHelper();
